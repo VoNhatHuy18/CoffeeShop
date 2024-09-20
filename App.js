00425ea187ce41179b165e87ecbb339434e92ea1
vonhathuy18 <vohuy177@gmail.com>
2024-09-20 14:05:45 +0700

    Màn hình giới thiệu

--- App.js
+++ App.js
@@ -5,13 +5,15 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import LoginScreen from "./src/Screens/LoginScreen";
 import RegisterScreen from "./src/Screens/RegisterScreen";
+import HomeScreen from "./src/Screens/HomeScreen";
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
